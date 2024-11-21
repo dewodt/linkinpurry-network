9997e45cca04e3275c72142f9e30c0ccae69f6a8
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-11-21 10:49:06 +0700

    feat: configure husky, eslint, prettier, nodemon, ect.

---
+++ backend/eslint.config.js
@@ -0,0 +1,39 @@
+// eslint.config.js
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['./**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    rules: {
+      // TypeScript specific rules
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+
+      // General ESLint rules
+      'no-console': 'warn',
+      eqeqeq: ['error', 'always'],
+      'no-unused-vars': 'off', // Turned off in favor of @typescript-eslint/no-unused-vars
+    },
+  },
+];
