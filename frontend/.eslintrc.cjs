215a4b550b3672c1d2ccf2b02f67c486311eb94e
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-11-23 22:00:43 +0700

    fix: git hooks.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // Added for strict TypeScript checks
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended', // Added for React Query best practices
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'coverage'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'], // Required for TypeScript rules
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    '@tanstack/query', // Added for React Query
+  ],
+  rules: {
+    // React Refresh
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+
+    // disable if something is weird lol
+
+    // TypeScript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+
+    // React Query
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/stable-query-client': 'error',
+
+    // General
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-unused-expressions': 'error',
+    'no-duplicate-imports': 'error',
+
+    // React Hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
