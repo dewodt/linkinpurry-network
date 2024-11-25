962db7c8cc95b3b3f799f0fefda9317c3b5450a7
Yusufarsan <sandiyusufardian@gmail.com>
2024-11-25 15:47:07 +0700

    feat: added fe for login and register page

--- frontend/src/routes/auth/login.tsx
+++ frontend/src/routes/auth/login.tsx
@@ -2,10 +2,25 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import * as React from 'react';
 
+import { LoginForm } from '@/components/login-form';
+
 export const Route = createFileRoute('/auth/login')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return 'Hello /auth/login!';
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <LoginForm />
+    </div>
+  );
+  // return 'Hello /auth/login!';
 }
+
+// export function Page() {
+//   return (
+//     <div className="flex h-screen w-full items-center justify-center px-4">
+//       <LoginForm />
+//     </div>
+//   )
+// }
