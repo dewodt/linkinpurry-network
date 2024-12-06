53ed7635403c9c2b008f9f5d8b66e82e48fb2373
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-12-06 22:39:45 +0700

    feat: basic dto for feed.

---
+++ backend/src/dto/feed-dto.ts
@@ -0,0 +1,186 @@
+import { z } from '@hono/zod-openapi';
+
+import { Utils } from '@/utils/utils';
+
+/**
+ * Get Feed timeline
+ */
+// request query
+export const getFeedTimelineRequestQueryDto = z.object({
+  cursor: z
+    .string({ message: 'cursor must be a string, bigint' })
+    .optional()
+    .refine((v) => v === undefined || Utils.parseBigIntId(v).isValid, {
+      message: 'cursor must be a string, bigint',
+    })
+    .transform((val) => (val !== undefined ? BigInt(val) : undefined))
+    .openapi({
+      description: 'Cursor for the next page',
+      example: '1',
+    }),
+  limit: z
+    .string({ message: 'limit must be a string' })
+    .optional()
+    .transform((val) => Utils.parseLimitPagination({ limit: val }))
+    .openapi({
+      description: 'Limit for pagination',
+      example: '15',
+    }),
+});
+
+export interface IGetFeedTimelineRequestQueryDto
+  extends z.infer<typeof getFeedTimelineRequestQueryDto> {}
+
+// response
+export const getFeedTimelineResponseBodyDto = z.object({
+  // added cursor here just do adjust to requirements. The FE will use the cursor from meta, not here.
+  // sucks, makes the response inconsistent but oh well :)
+  cursor: z.string().nullable().openapi({
+    description: 'Cursor for the next page',
+    example: '1',
+  }),
+
+  // actual data (paginated)
+  data: z.array(
+    z.object({
+      feed_id: z.string().openapi({
+        description: 'ID of the feed',
+        example: '12345',
+      }),
+      user_id: z.string().openapi({
+        description: 'ID of the user',
+        example: '67890',
+      }),
+      username: z.string().openapi({
+        description: 'Username of the user',
+        example: 'dewodt',
+      }),
+      full_name: z.string().openapi({
+        description: 'Full name of the user',
+        example: 'John Doe',
+      }),
+      profile_photo_path: z.string().openapi({
+        description: 'Profile photo of the user',
+        example: 'https://example.com/image.jpg',
+      }),
+      content: z.string().openapi({
+        description: 'Content of the post',
+        example: 'Hello world!',
+      }),
+      created_at: z.string().openapi({
+        description: 'Timestamp of the post',
+        example: '2021-09-01T00:00:00.000Z',
+      }),
+    })
+  ),
+});
+
+export interface IGetFeedTimelineResponseBodyDto
+  extends z.infer<typeof getFeedTimelineResponseBodyDto> {}
+
+/**
+ * Get my feed (no api contract provided, create own.)
+ */
+// Request query
+export const getMyFeedRequestQueryDto = z.object({
+  search: z.string({ message: 'search must be type of string' }).optional().openapi({
+    description: 'Search query for filtering current user posts',
+    example: 'John Doe',
+  }),
+  page: z.string({ message: 'page must be type of string' }).optional().openapi({
+    description: 'Page number for pagination',
+    example: '1',
+  }),
+  limit: z.string({ message: 'limit must be type of string' }).optional().openapi({
+    description: 'Limit for pagination',
+    example: '15',
+  }),
+});
+
+export interface IGetMyFeedRequestQueryDto extends z.infer<typeof getMyFeedRequestQueryDto> {}
+
+// Response
+export const getMyFeedResponseBodyDto = z.array(
+  z.object({
+    feed_id: z.string().openapi({
+      description: 'ID of the feed',
+      example: '12345',
+    }),
+    content: z.string().openapi({
+      description: 'Content of the post',
+      example: 'Hello world!',
+    }),
+    created_at: z.string().openapi({
+      description: 'Timestamp of the post',
+      example: '2021-09-01T00:00:00.000Z',
+    }),
+  })
+);
+
+export interface IGetMyFeedResponseBodyDto extends z.infer<typeof getMyFeedResponseBodyDto> {}
+
+/**
+ * Create post
+ */
+// Request body
+export const createPostRequestBodyDto = z.object({
+  content: z
+    .string({ message: 'Content is required' }) // handle null or undefined
+    .min(1, { message: 'Content is required' }) // handle empty string
+    .max(280, { message: 'Content maximum length is 280 characters' })
+    .openapi({
+      description: 'Post content',
+      example: 'Hello world!',
+    }),
+});
+
+export interface ICreatePostRequestBodyDto extends z.infer<typeof createPostRequestBodyDto> {}
+
+// Response
+// No need response, invalidate the query
+
+/**
+ * Update post
+ */
+// Request params
+export const updatePostRequestParamsDto = z.object({
+  feed_id: z.string({ message: 'feed_id must be a string' }).openapi({
+    description: 'ID of the feed',
+    example: '12345',
+  }),
+});
+
+export interface IUpdatePostRequestParamsDto extends z.infer<typeof updatePostRequestParamsDto> {}
+
+// Request body
+export const updatePostRequestBodyDto = z.object({
+  content: z
+    .string({ message: 'Content is required' }) // handle null or undefined
+    .min(1, { message: 'Content is required' }) // handle empty string
+    .max(280, { message: 'Content maximum length is 280 characters' })
+    .openapi({
+      description: 'Post content',
+      example: 'Hello world!',
+    }),
+});
+
+export interface IUpdatePostRequestParamsDto extends z.infer<typeof updatePostRequestParamsDto> {}
+
+// Response
+// No need response, invalidate the query
+
+/**
+ * Delete post
+ */
+// Request params
+export const deletePostRequestParamsDto = z.object({
+  feed_id: z.string({ message: 'feed_id must be a string' }).openapi({
+    description: 'ID of the feed',
+    example: '12345',
+  }),
+});
+
+export interface IDeletePostRequestParamsDto extends z.infer<typeof deletePostRequestParamsDto> {}
+
+// Response
+// No need response, invalidate the querys
